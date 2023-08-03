d47cf2cf865588192364471aa278181259ec791d
Igorbrito77 <britoigor77@gmail.com>
2023-08-03 18:00:04 -0300

    Adicionando lista suspensa

--- src/componentes/Formulario/Formulario.js
+++ src/componentes/Formulario/Formulario.js
@@ -1,7 +1,17 @@
 import CampoTexto from "../CampoTexto";
+import ListaSuspensa from "../ListaSuspensa";
 import './Formulario.css';
 
 export const Formulario = (props) =>{
+
+    const times = [
+        'Programação',
+        'Front-End',
+        'Back-End',
+        'Mobile',
+        'BigData',
+        'Inovação e Gestão'
+    ];
 
     return (
         <section className="formulario">
@@ -10,6 +20,7 @@ export const Formulario = (props) =>{
                 <CampoTexto label="Nome" placeholder="Digite o seu nome"/>
                 <CampoTexto label="Cargo" placeholder="Digite o seu cargo"/>
                 <CampoTexto label= "Imagem" placeholder="Digite o endereço da imagem"/>
+                <ListaSuspensa label= "Time" itens={times}/>
             </form>
 
         </section>
