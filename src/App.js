3d5c1afd9485893d43fd801c5dde757067c6a090
Igorbrito77 <britoigor77@gmail.com>
2023-08-04 18:16:55 -0300

    Adicionando sessões dos times

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { useState } from 'react';
 import Banner from './componentes/Banner';
 import { Formulario } from './componentes/Formulario/Formulario';
+import { Time } from './componentes/Time/Time';
 
 function App() {
 
@@ -16,6 +17,11 @@ function App() {
 
       <Banner/>
       <Formulario aoColaboradorCadastrado={colaborador  => aoNovoColaboradorAdicionado(colaborador)}/>
+      <Time nome="Programação"/>
+      <Time nome="Back-End"/>
+      <Time nome="Front-End"/>
+
+
     </div>
   );
 }
